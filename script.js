1cf68093c2b79e9d812b04cf60709ea54f0d8cde
Elena <47466926+ElenaKosobrodova@users.noreply.github.com>
2020-02-22 22:38:07 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,65 @@
-  function sendRequest(){
-    var inputVal = $('#search-input').val();
-    var url = "https://en.wikipedia.org/w/api.php?callback=?&action=opensearch&origin=*&search=" + inputVal + "&format=json"; 
-  console.log(url);
+function sendRequest() {
+  var inputVal = $("#search-input").val();
+  var url =
+    "https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&srsearch=" +
+    inputVal +
+    "&format=json";
   $.ajax({
-      url:  url,
-      dataType: 'json',
-      type: 'POST',
-      headers: { 'Api-User-Agent': 'codepen.io Wiki Viewer by Elena Kosobrodova' },   
-      success: function(res){
-       var wikiRes={
-         title: res[1],
-         article: res[2],
-         urla: res[3]
-       }; 
-       render(wikiRes); 
-      },
-      error: function(err){
-        console.log("error");
+    url: url,
+    dataType: "json",
+    type: "POST",
+    headers: {
+      "Api-User-Agent": "codepen.io Wiki Viewer by Elena Kosobrodova"
+    },
+    success: function(res) {
+      var title1 = [];
+      var article1 = [];
+      for (i = 0; i < res.query.search.length; i++) {
+        title1.push(res.query.search[i].title);
+        article1.push(res.query.search[i].snippet);
       }
-    });
-  };
-
-function render(data){
-  for (i=0; i<data.title.length; i++){
-    var item1 = "<div class='box'><h4 class='title'>" + data.title[i] + "</h4><p>" + data.article[i] + "</p></div></div>" 
-    $('#response').append(item1);
-  }
+      var wikiRes = {
+        title: title1,
+        article: article1
+      };
+      render(wikiRes);
+    },
+    error: function(err) {
+      console.log("error");
     }
+  });
+}
+
+function render(data) {
+  for (i = 0; i < data.title.length; i++) {
+    var item1 =
+      "<div class='box'><h4 class='title'>" +
+      data.title[i] +
+      "</h4><p>" +
+      data.article[i] +
+      "</p></div></div>";
+    $("#response").append(item1);
+  }
+}
 
 $(document).ready(function() {
   // random
   $("#random-article").on("click", function() {
- window.open('http://en.wikipedia.org/wiki/Special:Random', '', "value _blank");
- });
- // search
-   
-  $('#search-icon').on("click", function() {
-    console.log('button clicked');
-     $('#response').html(" ");
+    window.open(
+      "http://en.wikipedia.org/wiki/Special:Random",
+      "",
+      "value _blank"
+    );
+  });
+  // search
+  $("#search-input").keypress(function(e) {
+    if (e.which == 13) {
+      $("#response").html(" ");
+      sendRequest();
+    }
+  });
+  $("#search-icon").on("click", function() {
+    $("#response").html(" ");
     sendRequest();
   });
-  });
+});
